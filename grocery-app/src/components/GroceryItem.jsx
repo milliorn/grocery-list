58052bc260a194e21f33047fd452fe2e7539cba0
milliorn <scottmilliorn@gmail.com>
2022-08-09 18:22:43 -0700

    delIcon

--- grocery-app/src/components/GroceryItem.jsx
+++ grocery-app/src/components/GroceryItem.jsx
@@ -6,15 +6,19 @@ const GroceryItem = ({ item, onDelete, onEdit }) => {
       <div className="item bg-zinc-100 rounded-md cursor-pointer flex justify-between	my-2 mx-auto max-w-full	overflow-scroll	py-3 px-5">
         <div>
           <p className="">
-            <span className="itemBold font-bold">Item Name:</span> {item.text}
+            <span className="itemBold font-bold">Item Name: </span> {item.text}
           </p>
           <p className="">
-            <span className="itemBold font-bold">Quantity:</span> {item.quantity}
+            <span className="itemBold font-bold">Quantity: </span>
+            {item.quantity}
           </p>
         </div>
         <div>
           <p>
-            <FaTimes onClick={() => onDelete(item.id)} className="delIcon" />
+            <FaTimes
+              onClick={() => onDelete(item.id)}
+              className="delIcon text-red-500 cursor-pointer"
+            />
           </p>
           <p>
             <FaPencilAlt onClick={() => onEdit(item.id)} className="editIcon" />
