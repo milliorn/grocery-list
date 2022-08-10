294d1dbec94fd57f12b134612a3cd03cf41164bb
milliorn <scottmilliorn@gmail.com>
2022-08-09 18:05:40 -0700

    .btn:focus

--- grocery-app/src/components/Button.jsx
+++ grocery-app/src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ color, text, onClick }) => {
     <button
       onClick={onClick}
       style={{ backgroundColor: color }}
-      className="btn bg-neutral-900 text-neutral-50 border rounded-md border-none	cursor-pointer inline-block	text-sm m-1 py-3 px-5 no-underline"
+      className="btn bg-neutral-900 text-neutral-50 border rounded-md border-none	cursor-pointer inline-block	text-sm m-1 py-3 px-5 no-underline focus:text-neutral-50 focus:outline-none"
     >
       {text}
     </button>
