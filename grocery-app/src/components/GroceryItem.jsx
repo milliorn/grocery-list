e450f387734a3ebdefb473b0599ed4af23822aa9
milliorn <scottmilliorn@gmail.com>
2022-08-10 20:28:20 -0700

    p text

--- grocery-app/src/components/GroceryItem.jsx
+++ grocery-app/src/components/GroceryItem.jsx
@@ -5,23 +5,23 @@ const GroceryItem = ({ item, onDelete, onEdit }) => {
     <div>
       <div className="flex justify-between max-w-full px-5 py-3 mx-auto my-2 overflow-scroll rounded-md cursor-pointer item bg-zinc-100">
         <div className="text-zinc-900">
-          <p>
+          <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
             <span className="font-bold itemBold">Item: </span>
             <span>{item.text}</span>
           </p>
-          <p>
+          <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
             <span className="font-bold itemBold">Quantity: </span>
             {item.quantity}
           </p>
         </div>
         <div>
-          <p>
+          <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
             <FaTimes
               onClick={() => onDelete(item.id)}
               className="my-1.5 text-red-500 cursor-pointer delIcon"
             />
           </p>
-          <p>
+          <p className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
             <FaPencilAlt
               onClick={() => onEdit(item.id)}
               className="my-1.5 text-blue-500 cursor-pointer editIcon"
