9561049c3485f253a4cbcfca38688008bd67306d
milliorn <scottmilliorn@gmail.com>
2022-10-29 14:47:14 -0700

    Missing an explicit type attribute for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@
 function Button({ color, text, onClick }: any): JSX.Element {
   return (
     <button
+      type="button"
       onClick={onClick}
       style={{ backgroundColor: color }}
       className="inline-block px-5 py-3 m-1 text-sm no-underline border border-none rounded-md cursor-pointer sm:text-base btn bg-zinc-900 text-zinc-50 focus:text-zinc-50 focus:outline-none md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl"
