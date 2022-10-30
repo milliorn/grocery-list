fea430ba0dfbcb961295659f176685ce7c4b7a8a
milliorn <scottmilliorn@gmail.com>
2022-10-29 18:40:58 -0700

    ItemEditDelete

---
+++ src/components/DeleteItem.tsx
@@ -0,0 +1,16 @@
+import { FaTimes } from "react-icons/fa";
+
+export function DeleteItem({
+  onDelete, item,
+}: {
+  onDelete: Function;
+  item: any;
+}): JSX.Element {
+  return (
+    <div className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+      <div className="my-1.5 text-red-500 cursor-pointer delIcon">
+        <FaTimes onClick={() => onDelete(item.id)} />
+      </div>
+    </div>
+  );
+}
