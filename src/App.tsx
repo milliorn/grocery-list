fb4c15d314fc6f863ea1dd6fec95f9603d176c82
milliorn <scottmilliorn@gmail.com>
2025-04-11 16:08:19 -0700

    moved min height into container div

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ function App(): JSX.Element {
   }
 
   return (
-    <div className="container max-w-2xl mx-auto my-0 overflow-auto text-zinc-50 opacity-95 bg-zinc-900 p-7">
+    <div className="container min-h-20 max-w-2xl mx-auto my-0 overflow-auto text-zinc-50 opacity-95 bg-zinc-900 p-7">
       <Header
         showForm={() => setShowItem(!showItem)}
         changeTextAndColor={showItem}
