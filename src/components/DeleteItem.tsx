d96fde8e6967a9cb4eb32d92636badf43e8c2117
Scott Milliorn <scottmilliorn@gmail.com>
2026-03-05 15:38:07 -0800

    Remove react-icons dependency and replace icons with SVGs in EditItem and DeleteItem components

--- src/components/DeleteItem.tsx
+++ src/components/DeleteItem.tsx
@@ -1,6 +1,5 @@
 import type { JSX } from "react"
 import type { DeleteItemProps } from "../props/DeleteItemProps"
-import { FaTimes } from "react-icons/fa"
 
 /**
  * DeleteItem component renders a clickable icon that triggers deletion of an item.
@@ -22,7 +21,9 @@ function DeleteItem({ onDelete, item }: DeleteItemProps): JSX.Element {
         className="my-1.5 text-red-500 cursor-pointer delIcon bg-transparent border-none p-0"
         onClick={() => onDelete(item.id)}
       >
-        <FaTimes />
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512" aria-hidden="true" fill="currentColor" width="1em" height="1em">
+          <path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z" />
+        </svg>
       </button>
     </div>
   )
